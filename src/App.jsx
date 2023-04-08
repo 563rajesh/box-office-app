1ed8713b4791a8580b5ece374d57a4a0e5d66c8c
rajesh <singhrajesh84657@gmail.com>
2023-04-09 00:20:00 +0530

    installed react-router-dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,29 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import Starred from './pages/Starred';
 function App() {
-  return <div className="App">hello</div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/starred" element={<Starred />}></Route>
+        <Route path="*" element={<div>Page not found</div>} />
+        {/* <Route path="/" element={<App />}>
+      <Route index element={<Home />} />
+      <Route path="teams" element={<Teams />}>
+        <Route path=":teamId" element={<Team />} />
+        <Route path="new" element={<NewTeamForm />} />
+        <Route index element={<LeagueStandings />} />
+      </Route>
+    </Route>
+    <Route element={<PageLayout />}>
+      <Route path="/privacy" element={<Privacy />} />
+      <Route path="/tos" element={<Tos />} />
+    </Route>
+    <Route path="contact-us" element={<Contact />} /> */}
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
---
+++ src/pages/Home.jsx
@@ -0,0 +1,6 @@
+import { Link } from 'react-router-dom';
+const Home = () => {
+  return <Link to="/starred">Go to starred page</Link>;
+};
+
+export default Home;
