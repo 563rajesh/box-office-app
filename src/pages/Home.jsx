f57abebb53b29360d7ea85783c0249270942d55d
rajesh <singhrajesh84657@gmail.com>
2023-04-11 02:23:26 +0530

    use useQuery on home page to fetch search data

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,24 +3,36 @@ import { searchForShows, searchForPeople } from '../api/apiGet';
 import SearchForm from '../component/SearchForm';
 import ShowsGrid from '../component/shows/ShowsGrid';
 import ActorsGrid from '../component/actors/ActorsGrid';
+import { useQuery } from 'react-query';
 
 const Home = () => {
-  const [apiData, setApiData] = useState(null);
-  const [apiDataError, setapiDataError] = useState(null);
+  const [filter, setFilter] = useState(null);
+  // const [apiData, setApiData] = useState(null);
+  // const [apiDataError, setapiDataError] = useState(null);
+  const { data: apiData, error: apiDataError } = useQuery({
+    queryKey: ['search', filter],
+    queryFn: () =>
+      filter.searchOption === 'shows'
+        ? searchForShows(filter.q)
+        : searchForPeople(filter.q),
+    enabled: !!filter,
+    refetchOnWindowFocus: false,
+  });
   const onSearch = async ({ q, searchOption }) => {
-    try {
-      setapiDataError(null);
+    setFilter({ q, searchOption });
+    // try {
+    //   setapiDataError(null);
 
-      let result;
-      if (searchOption === 'shows') {
-        result = await searchForShows(q);
-      } else {
-        result = await searchForPeople(q);
-      }
-      setApiData(result);
-    } catch (error) {
-      setapiDataError(error);
-    }
+    //   let result;
+    //   if (searchOption === 'shows') {
+    //     result = await searchForShows(q);
+    //   } else {
+    //     result = await searchForPeople(q);
+    //   }
+    //   setApiData(result);
+    // } catch (error) {
+    //   setapiDataError(error);
+    // }
   };
   const renderApiData = () => {
     if (apiDataError) {
